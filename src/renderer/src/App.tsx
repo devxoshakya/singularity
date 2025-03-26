d7f3cbcf880cc62f241049bdc9a013ba806f18a0
Dev Shakya <devxoshakya@gmail.com>
2025-03-26 19:21:28 +0530

    fix the app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,55 +1,12 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { useState } from 'react'
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom'
-import MAC from './hooks/MacComp'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+// import MAC from './hooks/MacComp'
 import { Dashboard } from './pages/dashboard'
-import ResultExtraction from './pages/ResultExtraction'
-import { Records } from './pages/Records'
+// import ResultExtraction from './pages/ResultExtraction'
+// import { Records } from './pages/Records'
 import Home from './pages/Home'
 
 function App(): JSX.Element {
-  // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-  const [counter, setCounter] = useState(0)
-
-  const increase = () => {
-    setCounter(counter + 1)
-  }
-
-  const decrease = () => {
-    setCounter(counter - 1)
-  }
-
-  function Hello(): JSX.Element {
-    return <h1>Hello World</h1>
-  }
-
-  function Main(): JSX.Element {
-    
-    return(
-      <div className="flex flex-row items-center my-auto justify-center h-screen p-16">
-      <Card className="w-[350px] my-auto">
-        <CardHeader>
-          <CardTitle>Counter: {counter}</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <p>Click on increase / decrease to check the state changes</p>
-          <Link to="/hello">
-          <p>Click Here To goto Hello page</p>
-          </Link>
-        </CardContent>
-        <CardFooter className="flex justify-between">
-          <Button onClick={decrease} variant="outline">
-            Decrease --
-          </Button>
-          <Button onClick={increase}>Increase ++</Button>
-        </CardFooter>
-      </Card>
-    </div>
-    
-    );
-  }
 
 
   return (
